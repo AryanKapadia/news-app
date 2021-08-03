6edd8e51fdaf659453d879f22e4978b3bf5c83a2
AryanKapadia <83091325+AryanKapadia@users.noreply.github.com>
2021-08-04 00:15:49 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,9 +134,9 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'kapadiaaryan01@gmail.com'
+DEFAULT_FROM_EMAIL = env.str("EMAIL")
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = env.str("PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
